7ad8f84bac4fc8f7a960df9177fd6f75c1011525
EduardoBBGusmao <42840856+EduardoBBGusmao@users.noreply.github.com>
2018-09-04 15:16:03 -0300

    Update questao_1.py

--- questoes/questao_1.py
+++ questoes/questao_1.py
@@ -12,7 +12,15 @@
 ##
 def main():
     print("questao 1")
-    
+    opcao = 'sim'
+
+    while opcao == 'sim' or opcao == 'Sim':
+	    salario = float(input("Digite o salário atual?\n"))
+	    aumento = float(input("Digite a porcentagem do aumento\n"))
+
+    	salario = salario*(100+aumento)/100
+    	print("Esse é o novo salário", salario)
+	    opcao = input("Deseja continuar? (Sim/Nao)\n")    
 
 
 if __name__ == '__main__':
