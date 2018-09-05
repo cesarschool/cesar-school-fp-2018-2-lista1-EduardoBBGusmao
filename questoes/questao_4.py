551589d28830d0654211e30891d453184cb9ef53
EduardoBBGusmao <42840856+EduardoBBGusmao@users.noreply.github.com>
2018-09-05 10:04:30 -0300

    Update questao_4.py

--- questoes/questao_4.py
+++ questoes/questao_4.py
@@ -12,7 +12,12 @@
 ##
 def main():
     print("questao 4")
+    Dias = int(input("Qual foi o número de dias que passou com o carro alugado?\n"))
+    Kms = float(input("Qual foi o número de quilometros rodados como o carro?\n"))
 
+    ValorTotal = Dias*60 + Kms*0.15
+
+    print("O valor total do aluguel foi de R$ ", ValorTotal)
 
     
 if __name__ == '__main__':
