0ee5b0a726e1b61a109e66f479e5276527dc10a3
EduardoBBGusmao <42840856+EduardoBBGusmao@users.noreply.github.com>
2018-09-05 10:05:21 -0300

    Update questao_5.py

--- questoes/questao_5.py
+++ questoes/questao_5.py
@@ -14,7 +14,13 @@
 ##
 def main():
     print("questao 5")
+    CigarrosPorDia = int(input("Digite quantos cigarros por dia o fumante fuma: \n"))
+    Anosfumados = float(input("Digite há quantos anos o fumante fuma: \n"))
 
+    TotalDeCigarros = CigarrosPorDia*(Anosfumados*365)
+    DiasPerdidos = (TotalDeCigarros*10/60)/24
+
+    print("Esse fumante já perdeu: ", DiasPerdidos, " dias.")
 
     
 if __name__ == '__main__':
